5d2d87f01b17dfc457e436c5739b417fcb4dec58
FancyJesse <jesus_andrade45@yahoo.com>
2020-10-21 10:12:09 -0700

    Added CDNs

--- public/idleuser.com/logout.php
+++ public/idleuser.com/logout.php
@@ -12,7 +12,7 @@
 	<link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
 	<link rel="shortcut icon" href="/assets/images/favicon.ico">
   <link rel="manifest" href="/assets/images/site.webmanifest">	
-  <link href="/assets/bootstrap-4.5.2-dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <link href="/assets/css/account.css" rel="stylesheet">
 
   <?php 
