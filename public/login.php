63c5292e455c201b19f7984928b34cecf401d98c
FancyJesse <jesus_andrade45@yahoo.com>
2020-12-15 21:50:29 -0800

    renamed simple css

--- public/login.php
+++ public/login.php
@@ -33,7 +33,7 @@
 	<link rel="shortcut icon" href="/assets/images/favicon.ico">
   <link rel="manifest" href="/assets/images/site.webmanifest">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link href="/assets/css/account.css" rel="stylesheet">
+  <link href="/assets/css/form.css" rel="stylesheet">
 
   <?php
     $meta = [
