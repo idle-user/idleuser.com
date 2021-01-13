ce42772d1e020a0a50bfb0f0b808787855da303e
FancyJesse <jesus_andrade45@yahoo.com>
2021-01-13 00:32:10 -0800

    removed hiding email

--- public/account/index.php
+++ public/account/index.php
@@ -1,7 +1,7 @@
 <?php 	require_once $_SERVER['DOCUMENT_ROOT'] . '/../src/session.php';  set_last_page();
 
 if(!$_SESSION['loggedin']){
-    redirect(0, '/login.php');
+    redirect(0, '/login');
     exit();
 }
 
@@ -50,10 +50,7 @@ $response = maybe_process_form();
           <label for="emailFormControlInput">Email Address</label>
           <span class="small text-muted float-right pt-3">Last Updated: <?php echo $userInfo['email_last_updated'];?></span>
           <div class="input-group mb-3">
-            <input type="password" class="form-control" id="emailFormControlInput" name="email" value="<?php echo $db->user_email($_SESSION['user_id'])['email']; ?>">
-            <div class="input-group-append">
-              <button class="btn btn-secondary" type="button" id="showHideButton" onclick="showHide()">Show</button>
-            </div>
+            <input type="email" class="form-control" id="emailFormControlInput" name="email" value="<?php echo $db->user_email($_SESSION['user_id'])['email']; ?>">
             <div class="input-group-append">
               <input class="btn btn-primary" type="submit" name="email-update" value="Update">
             </div>
@@ -61,22 +58,9 @@ $response = maybe_process_form();
         </div>
       </form>
 
-      <a class="btn btn-primary mt-5" type="button" href="change-password.php">Change Password</a>
+      <a class="btn btn-primary mt-5" type="button" href="/change-password">Change Password</a>
 
       <?php include 'includes/footer.php'; ?>
     </main>
-    <script>
-      function showHide() {
-        var x = document.getElementById("emailFormControlInput");
-        var y = document.getElementById("showHideButton");
-        if (x.type === "password") {
-          x.type = "email";
-          y.textContent = "Hide";
-        } else {
-          x.type = "password";
-          y.textContent = "Show";
-        }
-      }
-    </script>
 </body>
 </html>
