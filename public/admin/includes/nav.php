9129b712d83563a840651a34232c43c5624fe350
FancyJesse <jesus_andrade45@yahoo.com>
2021-12-14 21:58:37 -0800

    grouped admin navbar pages to dropdowns

--- public/admin/includes/nav.php
+++ public/admin/includes/nav.php
@@ -1,15 +1,27 @@
 <?php include '../includes/nav.php'; ?>
 
-<div class="nav-scroller bg-white shadow-sm">
-    <nav class="nav nav-underline">
-        <a class="nav-link active" href="index" title="Don't fuck shit up.">Admin Dashboard</a>
-        <a class="nav-link" href="matches-editor">Matches Editor</a>
-        <a class="nav-link" href="matches-metadata">Matches Metadata</a>
-        <a class="nav-link" href="matches-roster">Matches Roster</a>
-        <a class="nav-link" href="matches-royalrumble">Matches Royal Rumble</a>
-        <a class="nav-link" href="discordbot-commands">IdleBot Commands</a>
-        <a class="nav-link" href="discordbot-scheduler">IdleBot Scheduler</a>
-        <a class="nav-link" href="analytics">Analytics</a>
 
-    </nav>
-</div>
+<nav class="nav nav-underline bg-white shadow-sm">
+    <a class="nav-link active" href="index" title="Don't fuck shit up.">Admin Dashboard</a>
+
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="matchesDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ignore-smoothscroll>Matches</a>
+        <div class="dropdown-menu" aria-labelledby="matchesDropdown">
+            <a class="nav-link" href="matches-editor">Matches Editor</a>
+            <a class="nav-link" href="matches-metadata">Matches Metadata</a>
+            <a class="nav-link" href="matches-roster">Matches Roster</a>
+            <a class="nav-link" href="matches-royalrumble">Matches Royal Rumble</a>
+        </div>
+    </li>
+
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="discordbotDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ignore-smoothscroll>DiscordBot</a>
+        <div class="dropdown-menu" aria-labelledby="matchesDropdown">
+            <a class="nav-link" href="discordbot-commands">IdleBot Commands</a>
+            <a class="nav-link" href="discordbot-scheduler">IdleBot Scheduler</a>
+        </div>
+    </li>
+
+    <a class="nav-link" href="analytics">Analytics</a>
+</nav>
+
